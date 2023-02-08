fd71bfad44c695d623cc05c3cc7fea9dfbf00165
steveGillet <stephen.gillet@gmail.com>
2023-02-08 10:31:26 -0600

    attempt to work perceptron 1

--- patternRec/assignment1continued.py
+++ patternRec/assignment1continued.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import numpy as np
+import matplotlib.pyplot as plt
 
 dataFrame = pd.read_excel('Proj1DataSet.xlsx')
 dataArray = dataFrame.to_numpy()
@@ -14,9 +15,31 @@ print(versivirgi)
 w = np.ones((1,5))
 print(w)
 wk = w
-rho = 0.01
+rho = 0.0001
 # print(np.sum(setosa[:,0]) + np.sum(-versivirgi[:,0]))
-wk = wk + rho*(np.array([np.sum(-versivirgi[:,0])+ np.sum(setosa[:,0]), np.sum(-versivirgi[:,1])+ np.sum(setosa[:,1]), np.sum(-versivirgi[:,2])+ np.sum(setosa[:,2]),np.sum(-versivirgi[:,3]) + np.sum(setosa[:,3]), 1]))
-wk = wk + rho*(np.array([np.sum(-versivirgi[:,0])+ np.sum(setosa[:,0]), np.sum(-versivirgi[:,1])+ np.sum(setosa[:,1]), np.sum(-versivirgi[:,2])+ np.sum(setosa[:,2]),np.sum(-versivirgi[:,3]) + np.sum(setosa[:,3]), 1]))
-wk = wk + rho*(np.array([np.sum(-versivirgi[:,0])+ np.sum(setosa[:,0]), np.sum(-versivirgi[:,1])+ np.sum(setosa[:,1]), np.sum(-versivirgi[:,2])+ np.sum(setosa[:,2]),np.sum(-versivirgi[:,3]) + np.sum(setosa[:,3]), 1]))
-print(wk)
+for i in range(200): 
+  wk = wk + rho*(np.array([np.sum(-versivirgi[:,0])+ np.sum(setosa[:,0]), np.sum(-versivirgi[:,1])+ np.sum(setosa[:,1]), np.sum(-versivirgi[:,2])+ np.sum(setosa[:,2]),np.sum(-versivirgi[:,3]) + np.sum(setosa[:,3]), 0]))
+
+print(versivirgi[:,0])
+print(np.sum(-versivirgi[:,0]))
+print(-np.sum(versivirgi[:,0]))
+print(wk.dot(np.array([np.sum(-versivirgi[:,0])+ np.sum(setosa[:,0]), np.sum(-versivirgi[:,1])+ np.sum(setosa[:,1]), np.sum(-versivirgi[:,2])+ np.sum(setosa[:,2]),np.sum(-versivirgi[:,3]) + np.sum(setosa[:,3]), 0])))
+
+wtest = np.random.rand(1,3)
+# np.append(wtest, np.array([1]), 1)
+plt.scatter(setosa[:,0],setosa[:,1], c='red')
+plt.scatter(virginica[:,0],virginica[:,1])
+# plt.show()
+
+for i in range(50): 
+  wtest = wtest + rho*(np.array([np.sum(setosa[:,0])+ np.sum(-virginica[:,0]), np.sum(setosa[:,1])+ np.sum(-virginica[:,1]), 0]))
+print(wtest)
+
+x0 = np.linspace(4,8)
+y = x0*wtest[0][0] + x0*wtest[0][1] + wtest[0][2]
+plt.plot(x0, y)
+plt.show()
+
+wlintest = np.linalg.pinv(np.array(setosa[:,0]))
+
+print(wtest[0][0])
