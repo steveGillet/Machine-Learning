3a430ef97aa0257439c3542be41d085afe96d4de
steveGillet <89035893+steveGillet@users.noreply.github.com>
2023-02-22 18:33:01 -0600

    up to test error

--- machineLearning2/Gillet_Steve_Project3.py
+++ machineLearning2/Gillet_Steve_Project3.py
@@ -1,35 +1,58 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
+testN = 1000
 L = 100
 N = 25
 s = 0.1
 X = np.random.uniform(0,1,(L,N))
 t = np.sin(2*np.pi*X) + np.random.normal(0,.3,(L,N))
-plt.scatter(X[0],t[0])
+x0 = np.linspace(0,1,N)
+hx = np.sin(2*np.pi*x0)
+Xtest = np.random.uniform(0,1,(testN))
+tTest = np.sin(2*np.pi*X) + np.random.normal(0,.3,(testN))
+# plt.plot(x0, hx)
 
-M = 2
+
+
+M = 4
 I = np.identity(M+1)
 lamb = np.linspace(0,3,N)
 phi = np.ones((N, M+1))
+yMean = np.zeros((N, N))
+bias = np.zeros((N, 1))
+variance = np.zeros((N, 1))
+testError = np.zeros((N, 1))
 
-for i in range(N):
-  for j in range(1,M+1):
-    phi[i,j] = np.exp((-(X[0,i]-(j)/(M+1))**2)/(2*s**2))
+for g in range(N):
+  y = np.zeros((L, N))
+  for h in range(L):
+    for i in range(N):
+      for j in range(1,M+1):
+        phi[i,j] = np.exp((-(X[h,i]-(j)/(M+1))**2)/(2*s**2))
 
-w = (np.linalg.inv(phi.transpose().dot(phi)+lamb[0]*I)).dot(phi.transpose()).dot(t[0].reshape(N,1))
+    w = (np.linalg.inv(phi.transpose().dot(phi)+lamb[g]*I)).dot(phi.transpose()).dot(t[h].reshape(N,1))
 
-y = np.ones(N)
-print(y)
-x0 = np.linspace(0,1,N)
-y[0] =w[0]
-for i in range(N):
-  for j in range(1,M+1):
-    y[i] += w[j]*np.exp((-(x0-j/(M+1))**2)/(2*s**2))
+    y[h]+=w[0]
 
-# y[j] = w[j][0]
-#         for k in range(1, m+1):
-#             y[j] += w[j][k]*np.exp(-((x0 - k/m)**2)/(2*s**2))
-# print(y)
-plt.plot(x0,y)
+    for j in range(1,M+1):
+      y[h] += w[j]*np.exp((-(x0-j/(M+1))**2)/(2*s**2))
+
+  yMean[g] = np.mean(y, axis=0)
+  for h in range(N):
+    bias[g] += (yMean[g, h]-hx[h])**2
+    for i in range(L):
+      variance[g] += (y[i,h] - yMean[g,h])**2
+    variance[g] /= L
+  bias[g] /= N
+  variance[g] /= N
+
+  # testError[g] = (np.sqrt((np.linalg.norm(phiTest.dot(w)-tTest, ord =2)**2)/testN))
+  
+  # plt.scatter(X,t,c='orange')
+  # plt.plot(x0,yMean[g], c='black')
+
+plt.plot(np.log(lamb), bias)
+plt.plot(np.log(lamb), variance)
+plt.plot(np.log(lamb), bias+variance)
 plt.show()
